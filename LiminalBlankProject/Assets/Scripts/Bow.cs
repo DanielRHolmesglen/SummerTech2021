b20440ee278243dcc629194916517f110c360995
Bomtill <80375642+Bomtill@users.noreply.github.com>
2022-01-14 14:39:15 +1100

    Bow Logic

--- LiminalBlankProject/Assets/Scripts/Bow.cs
+++ LiminalBlankProject/Assets/Scripts/Bow.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
@@ -8,8 +10,10 @@ public class Bow : MonoBehaviour
     public GameObject ArrowMesh; // for holding the arrow in the bow
 
     public float grabThreshold = 0.15f;
-    public Transform arrowGrabPoint;
+    public BoxCollider arrowGrabPoint;
+    public BoxCollider bowGrabPoint;
     public Transform fullDrawPoint;
+    public Transform startPoint;
     public Transform arrowSocket;
 
     private Transform pullingHand;
@@ -18,24 +22,36 @@ public class Bow : MonoBehaviour
 
     private float pullValue = 0.0f;
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CreateArrow(0.0f));
+        animator = GetComponent<Animator>();
+        StartCoroutine(CreateDummyArrow(0.0f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!pullingHand || !currentArrow) return;
+        pullValue = CalculatePull(pullingHand);
+        pullValue = Mathf.Clamp(pullValue, 0, 1);
+
+        animator.SetFloat("Blend", pullValue);
     }
 
-    private IEnumerator CreateArrow(float waitTime)
+    private float CalculatePull(Transform pullHand)
+    {
+        Vector3 direction = fullDrawPoint.position - startPoint.position;
+        float magnitude = direction.magnitude;
+
+        direction.Normalize();
+        Vector3 difference = pullHand.position - startPoint.position;
+
+        return Vector3.Dot(difference, direction) / magnitude;
+    }
+
+    private IEnumerator CreateDummyArrow(float waitTime)
     {
         // wait
         yield return new WaitForSeconds(waitTime);
@@ -47,11 +63,27 @@ public class Bow : MonoBehaviour
 
     public void Pull(Transform hand)
     {
+        float distance = Vector3.Distance(hand.position, startPoint.position);
 
+        if (distance > grabThreshold) return;
+
+        pullingHand = hand;
     }
 
     public void Release()
     {
+        if (pullValue > 0.25f) FireArrow();
 
+        pullingHand = null;
+
+        pullValue = 0;
+        animator.SetFloat("Blend", 0);
+
+        if (!currentArrow)
+            StartCoroutine(CreateDummyArrow(0.25f));
+    }
+    private void FireArrow()
+    {
+        ArrowPrefab = null;
     }
 }
