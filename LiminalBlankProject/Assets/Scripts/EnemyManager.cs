2cf5903703290781903fa7bb706574095400bed8
Dcaf944 <thomas100@live.com.au>
2022-01-18 14:35:44 +1100

    BasicEnemyManage
    We have some sort of spawn and enemy movement

---
+++ LiminalBlankProject/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public List<GameObject> spawnPositions;
+    public GameObject player;
+    public float speed = 1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject enemy = Instantiate(enemyPrefab, spawnPositions[Random.Range(0, 4)].transform.position, enemyPrefab.transform.rotation);
+            enemy.transform.LookAt(player.transform);
+            StartCoroutine(SendHoming(enemy));
+        }
+    }
+
+    public IEnumerator SendHoming(GameObject enemy)
+    {
+        while (Vector3.Distance(player.transform.position, enemy.transform.position) > 0.3)
+        {
+            enemy.transform.position += (player.transform.position - enemy.transform.position).normalized * speed * Time.deltaTime;
+            enemy.transform.LookAt(player.transform);
+            yield return null;
+        }
+        Destroy(enemy);
+    }
+
+}
