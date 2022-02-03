35fba27f455120a317102670c1ecd93076a11079
Dcaf944 <thomas100@live.com.au>
2022-02-04 03:05:02 +1100

    Teleport SFX and smoothing
    Teleport SFX and smoothing

--- LiminalBlankProject/Assets/Scripts/PlayerTeleport.cs
+++ LiminalBlankProject/Assets/Scripts/PlayerTeleport.cs
@@ -6,18 +6,53 @@ public class PlayerTeleport : MonoBehaviour
 {
     public Transform playerPos;
     public Transform playerTeleportLocation;
+    public GameObject fadeUI;
+    public GameObject teleportSFX;
+    private bool teleport = false;
+
+    private void Start()
+    {
+        //fadeUI.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (teleport == true)
+        {
+            teleportSFX.SetActive (true);
+            fadeUI.SetActive(true);
+            Debug.Log("Teleport");
+            playerPos.position = Vector3.Lerp(playerPos.position, playerTeleportLocation.position, 0.4f);
+            Invoke("StopTeleport", 0.45f);
+        }
+
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.tag == "Arrow")
         {
-            playerPos.position = playerTeleportLocation.position;
-            Destroy(gameObject);
+            teleport = true;
+           // playerPos.position = playerTeleportLocation.position;
+           // Destroy(gameObject);
         }
 
     }
 
+    void StopTeleport()
+    {
+        Invoke("EndFade", 0.45f);
+        teleport = false;
+        Destroy(gameObject);
+    }
 
+    void EndFade()
+    {
+        teleportSFX.SetActive(false);
+        fadeUI.SetActive(false);
+    }
 
 }
